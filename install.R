01818f12d525b524f3182c2f093fbfefa1663535
Ana Diedrichs (yoga) <ana.diedrichs@frm.utn.edu.ar>
2020-09-28 10:55:12 -0300

    related #1

---
+++ install.R
@@ -0,0 +1,4 @@
+install.packages("devtools")
+devtools::install_version(package = "dplyr", version = "1.0.0")
+devtools::install_version(package = "stringr", version = "1.4.0")
+devtools::install_version(package = "guaguas", version = "0.1.0")
